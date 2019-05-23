c03d84cea535bf4e86d40516cdee08a6ad19ed53
teemal <taymal1987@gmail.com>
2019-05-22 20:32:56 -0700

    chapter 2 sort of finished?
    errors in text making me not sure how to go about this. Will get clarification tomorrow (5/23/19)

--- ch_one_two.pl
+++ ch_one_two.pl
@@ -86,3 +86,92 @@ wizard(Y).  - ron
 witch(Y).  - undefined witch
 
  */
+
+/*
+
+Exercise  2.1 Which of the following pairs of terms unify? Where relevant, give the variable instantiations that lead to successful unification.
+
+bread  =  bread  - true
+’Bread’  =  bread - false
+’bread’  =  bread - true
+Bread  =  bread - true Bread = bread.
+bread  =  sausage - false
+food(bread)  =  bread - false
+food(bread)  =  X - true X = food(bread).
+food(X)  =  food(bread) - true X = bread.
+food(bread,X)  =  food(Y,sausage) - true X = sausage, Y = bread.
+food(bread,X,beer)  =  food(Y,sausage,X) - false
+food(bread,X,beer)  =  food(Y,kahuna_burger) - false
+food(X)  =  X - true X = food(X).
+meal(food(bread),drink(beer))  =  meal(X,Y) - true X = food(bread), Y = drink(beer).
+meal(food(bread),X)  =  meal(X,drink(beer)) - false
+
+*/
+
+/*
+Exercise  2.2 We are working with the following knowledge base:
+*/
+/*
+   house_elf(dobby). 
+   witch(hermione). 
+   witch(’McGonagall’).  %this throws an error
+   witch(rita_skeeter). 
+   magic(X):-  house_elf(X). 
+   magic(X):-  wizard(X). 
+   magic(X):-  witch(X).
+*/
+/*
+Which of the following queries are satisfied? Where relevant, give all the variable instantiations that lead to success.
+
+?-  magic(house_elf). - every one of these throws an error like this "Undefined procedure: wizard/1"
+?-  wizard(harry).
+?-  magic(wizard).
+?-  magic(’McGonagall’).
+?-  magic(Hermione).
+
+Draw the search tree for the query magic(Hermione) 
+
+.....um....I can't?
+
+*/
+
+/*
+
+Exercise  2.3 Here is a tiny lexicon (that is, information about individual words) and a mini grammar consisting of one syntactic rule (which defines a sentence to be an entity consisting of five words in the following order: a determiner, a noun, a verb, a determiner, a noun).
+
+*/
+/*
+   word(determiner,a). 
+   word(determiner,every). 
+   word(noun,criminal). 
+   word(noun,’big  kahuna  burger’). %this line throws an error
+   word(verb,eats). 
+   word(verb,likes). 
+
+   sentence(Word1,Word2,Word3,Word4,Word5):- 
+         word(determiner,Word1),  - throws an error so not entirely sure what to do
+         word(noun,Word2), 
+         word(verb,Word3), 
+         word(determiner,Word4), 
+         word(noun,Word5).
+What query do you have to pose in order to find out which sentences the grammar can generate? List all sentences that this grammar can generate in the order that Prolog will generate them in.
+*/
+/*
+Exercise  2.4 Here are six Italian words:
+
+astante , astoria , baratto , cobalto , pistola , statale .
+
+They are to be arranged, crossword puzzle fashion, in the following grid:
+
+
+The following knowledge base represents a lexicon containing these words:
+
+   word(astante,  a,s,t,a,n,t,e). 
+   word(astoria,  a,s,t,o,r,i,a). 
+   word(baratto,  b,a,r,a,t,t,o). 
+   word(cobalto,  c,o,b,a,l,t,o). 
+   word(pistola,  p,i,s,t,o,l,a). 
+   word(statale,  s,t,a,t,a,l,e).
+Write a predicate crossword/6 that tells us how to fill in the grid. The first three arguments should be the vertical words from left to right, and the last three arguments the horizontal words from top to bottom.
+
+*/
