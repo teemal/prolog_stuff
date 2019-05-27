cbe5b5bdc0781256d8d286275513bb8571e7bada
teemal <taymal1987@gmail.com>
2019-05-26 17:08:08 -0700

    Ch 3: finished up to practical #2
    TODO: practical 3 & 4 and then all of Ch 4

--- junk.pl
+++ junk.pl
@@ -1,6 +1,34 @@
-loves(vincent,mia). 
-loves(marsellus,mia). 
-loves(pumpkin,honey_bunny). 
-loves(honey_bunny,pumpkin). 
+authors('harry potter', 'jkrowling').
+authors('harry potter fanfic', 'jkrowling').
+authors('got','rrmartin').
 
-jealous(X,Y):-  loves(X,Z),  loves(Y,Z).
+musician_instrument('celine dion', voice).
+musician_instrument('zac wild', guitar).
+musician_instrument('eric clapton', guitar).
+musician_instrument('neil pert', drums).
+
+musician_genre('celine dion', bad).
+musician_genre('zac wild', metal).
+musician_genre('eric clapton', classic_rock).
+musician_genre('neil pert', progressive_rock).
+
+artists_paintings_and_style('van gogh', 'stary night', impressionism).
+artists_paintings_and_style('monet', 'lilly pads', impressionism).
+
+diff(red, blue).
+diff(red, green).
+diff(green, red).
+diff(green, blue).
+diff(blue, green).
+diff(blue, red).
+
+coloring(A, F, G, M, T):-
+    diff(A,F),
+    diff(A,G),
+    diff(A,M),
+    diff(A,T),
+    diff(F,G),
+    diff(G,T),
+    diff(T,M).
+
+
---
+++ mallory_PrologCh3Ch4Exercises.pl
@@ -0,0 +1,160 @@
+/* 
+created by Taylor Mallory
+for CS381 Programming Language Fundamentals
+
+Chapters 3 and 4
+*/
+
+/*
+Exercise  3.1 In the text, we discussed the predicate
+
+   descend(X,Y)  :-  child(X,Y). 
+   descend(X,Y)  :-  child(X,Z), 
+                                     descend(Z,Y).
+Suppose we reformulated this predicate as follows:
+
+   descend(X,Y)  :-  child(X,Y). 
+   descend(X,Y)  :-  descend(X,Z), 
+                                     descend(Z,Y).
+Would this be problematic?
+
+yes, you'd run into an inifitely recursive call
+
+*/
+
+/*
+3.2 Do you know these wooden Russian dolls (Matryoshka dolls) where the smaller ones are contained in bigger ones? Here is a schematic picture:
+
+First, write a knowledge base using the predicate directlyIn/2 which encodes which doll is directly contained in which other doll. Then, define a recursive predicate in/2 , that tells us which doll is (directly or indirectly) contained in which other dolls. For example, the query in(katarina,natasha) should evaluate to true, while in(olga,  katarina) should fail.
+
+*/
+
+directlyIn(katarina, olga).
+directlyIn(olga, natasha).
+directlyIn(natasha,irina).
+
+in(X, Y) :-
+    directlyIn(X, Y).
+in(X, Y) :-
+    directlyIn(X, Z),
+    in(Z, Y).
+
+/*
+3.3 We have the following knowledge base:
+*/
+   directTrain(saarbruecken,dudweiler). 
+   directTrain(forbach,saarbruecken). 
+   directTrain(freyming,forbach). 
+   directTrain(stAvold,freyming). 
+   directTrain(fahlquemont,stAvold). 
+   directTrain(metz,fahlquemont). 
+   directTrain(nancy,metz).
+
+/*
+That is, this knowledge base holds facts about towns it is possible to travel between by taking a direct train. But of course, we can travel further by chaining together direct train journeys. Write a recursive predicate travelFromTo/2 that tells us when we can travel by train between two towns. For example, when given the query
+
+   travelFromTo(nancy,saarbruecken).
+it should reply yes.
+*/
+
+travelFromTo(X, Y) :-
+    directTrain(X, Y).
+travelFromTo(X, Y) :-
+    directTrain(X, Z),
+    travelFromTo(Z, Y).
+
+/*
+3.4 Define a predicate greater_than/2 that takes two numerals in the notation that we introduced in the text (that is, 0, succ(0), succ(succ(0)), and so on) as arguments and decides whether the first one is greater than the second one. For example:
+
+   ?-  greater_than(succ(succ(succ(0))),succ(0)). 
+   yes 
+   ?-  greater_than(succ(succ(0)),succ(succ(succ(0)))). 
+   no
+*/
+
+greater_than(succ(_), 0).
+greater_than(succ(X), succ(Y)) :-
+    greater_than(X, Y).
+
+/*
+3.5 Binary trees are trees where all internal nodes have exactly two children. The smallest binary trees consist of only one leaf node. We will represent leaf nodes as leaf(Label) . For instance, leaf(3) and leaf(7) are leaf nodes, and therefore small binary trees. Given two binary trees B1 and B2 we can combine them into one binary tree using the functor tree/2 as follows: tree(B1,B2) . So, from the leaves leaf(1) and leaf(2) we can build the binary tree tree(leaf(1),leaf(2)) . And from the binary trees tree(leaf(1),leaf(2)) and leaf(4) we can build the binary tree tree(tree(leaf(1),  leaf(2)),leaf(4)) .
+
+Now, define a predicate swap/2 , which produces the mirror image of the binary tree that is its first argument. For example:
+
+   ?-  swap(tree(tree(leaf(1),  leaf(2)),  leaf(4)),T). 
+   T  =  tree(leaf(4),  tree(leaf(2),  leaf(1))). 
+   yes
+*/
+
+swap(leaf(X), leaf(X)).
+swap(tree(A,B), tree(C,D)) :-
+    swap(C,B),
+    swap(A,D).
+
+/*
+3.4 - 1
+Write a predicate path/2 that tells you from which points in the maze you can get to which other points when chaining together connections given in the above knowledge base. Can you get from point 5 to point 10? Which other point can you get to when starting at point 1? And which points can be reached from point 13?
+*/
+
+   connected(1,2). 
+   connected(3,4). 
+   connected(5,6). 
+   connected(7,8). 
+   connected(9,10). 
+   connected(12,13). 
+   connected(13,14). 
+   connected(15,16). 
+   connected(17,18). 
+   connected(19,20). 
+   connected(4,1). 
+   connected(6,3). 
+   connected(4,7). 
+   connected(6,11). 
+   connected(14,9). 
+   connected(11,15). 
+   connected(16,12). 
+   connected(14,17). 
+   connected(16,19).
+
+path(A, B) :-
+    connected(A, B).
+path(A, B) :-
+    connected(A, Z),
+    path(Z, B).
+
+/* 
+3.4 - 2
+Write a predicate travel/2 which determines whether it is possible to travel from one place to another by chaining together car, train, and plane journeys. For example, your program should answer yes to the query travel(valmont,raglan) .
+*/
+
+   byCar(auckland,hamilton). 
+   byCar(hamilton,raglan). 
+   byCar(valmont,saarbruecken). 
+   byCar(valmont,metz). 
+    
+   byTrain(metz,frankfurt). 
+   byTrain(saarbruecken,frankfurt). 
+   byTrain(metz,paris). 
+   byTrain(saarbruecken,paris). 
+    
+   byPlane(frankfurt,bangkok). 
+   byPlane(frankfurt,singapore). 
+   byPlane(paris,losAngeles). 
+   byPlane(bangkok,auckland). 
+   byPlane(singapore,auckland). 
+   byPlane(losAngeles,auckland).
+
+goTo(A, B) :-
+    byCar(A, B);
+    byPlane(A, B);
+    byTrain(A, B).
+travel(A, B) :-
+    goTo(A, B).
+travel(A, B) :-
+    goTo(A, X),
+    travel(X, B).
+
+
+
+
+
